aa00c549597570ec43e1128a23c186e03edea095
Praveen Pal <praveenpal4232@gmail.com>
2019-09-21 00:06:04 +0530

    update

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,8 @@
-self.addEventListener('install', function(event) {
-    // Perform install steps
-    var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-'/',
-  '/Google-Gifts/',
-  '/Google-Gifts/css/main.css',
-  '/Google-Gifts/js/main.js'
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
 ];
 
 self.addEventListener('install', function(event) {
@@ -18,4 +15,3 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-  });
