34c46ec20a853c14e00c5fefb7ed66cd4366c5d7
Praveen Pal <praveenpal4232@gmail.com>
2019-09-20 22:54:26 +0530

    Perform install steps

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,20 @@
 self.addEventListener('install', function(event) {
     // Perform install steps
+    var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/Google-Gifts/',
+  '/css/main.css',
+  '/js/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
   });
