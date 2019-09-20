d8261e410dd1a93993b838857b4814f68fd73c7f
Praveen Pal <praveenpal4232@gmail.com>
2019-09-20 23:58:56 +0530

    update

--- js/sw.js
+++ js/sw.js
@@ -2,6 +2,7 @@ self.addEventListener('install', function(event) {
     // Perform install steps
     var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+'/',
   '/Google-Gifts/',
   '/Google-Gifts/css/main.css',
   '/Google-Gifts/js/main.js'
