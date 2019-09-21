1dbe498e296e5aff08d9a709068bcc43934a0a08
Praveen Pal <praveenpal4232@gmail.com>
2019-09-21 23:27:34 +0530

    update

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
       navigator.serviceWorker.register('/Google-Gifts/js/sw.js').then(function(registration) {
         // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('Hello Folks! I am Praveen and good to see you.', registration.scope);
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
